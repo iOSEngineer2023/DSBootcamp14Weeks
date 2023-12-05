823a2598a6b49a3caabcffa426acf32533f72ead
iOSEngineer2023 <iosengineer2023@proton.me>
2023-12-05 23:05:17 +0200

    - add day2 challenge

--- day2.playground/Contents.swift
+++ day2.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var name = "X"
+let username = "adminn"
+let password = "password1234"
+
+//Create Login System
+
+
+if username == "admin" & password == "password123" {
+    print("accees granted")
+} else {
+    print("Access denied!")
+}
+
+// Initialize Variables
+
+var totalCost: Double = 100.0
+var taxRate: Double = 0.19
+var discount: Double = 0.10
+
