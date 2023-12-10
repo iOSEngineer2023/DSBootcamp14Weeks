8e61b1d0da8f95d2b07e5d402386335349e8d030
iOSEngineer2023 <iosengineer2023@proton.me>
2023-12-10 18:43:04 +0100

    - add day7 challenge

--- day6.playground/Contents.swift
+++ day6.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 //Day 6: Challenge 1 For-In Loop - Sum of Digits
 
-func sumOfDigitis(_ number: Int) -> Int {
+func sumOfDigits(_ number: Int) -> Int {
     let stringNumber = String(number)
     var sum = 0
 
@@ -14,12 +14,29 @@ func sumOfDigitis(_ number: Int) -> Int {
     return sum
 }
 
-// Example
-let result = sumOfDigits(12345) // = 15
-print(result)
+let result = sumOfDigits(12345)
+print("You're \(result) result")
 
 
 //Challenge 2
+
+
+func guessTheGame(guesses: [Int]){
+    let randomNumber = Int.random(in: 1...10)
+    print("Random number is \(randomNumber)")
+
+    for guess in guesses {
+        print("Print your guess: \(guess)")
+        if guess == randomNumber{
+            print("Correct! The number was \(randomNumber)")
+        }else{
+            print("Wrong! Try again!")
+        }
+    }
+    print("No more guesses left. The number was \(randomNumber)")
+}
+
+
 import Foundation
 
 func guessingGame(guesses: [Int]) {
@@ -41,3 +58,5 @@ func guessingGame(guesses: [Int]) {
 
 // Example
 guessingGame(guesses: [3, 7, 1, 10])
+
+
